506dfebe3145c90966198d0c782a7e42e0dd570b
Miss Taco <misstaco@msn.com>
2025-06-03 21:29:46 -0400

    update: token

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const compileOverleaf = require('./src');
 
 try {
-    // https://overleaf.com/read/fjspyrhfrsgc
-    const token = 'fjspyrhfrsgc';
+    // https://overleaf.com/read/bsftpwmbcznd
+    const token = 'bsftpwmbcznd';
     compileOverleaf(token).then((res) => {
         /**
          * {
